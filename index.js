54e792d9ce3f53c7ef347c1d1152e06b65872364
rupam170123041 <sahurupam0@gmail.com>
2020-07-05 14:24:54 +0530

    css changed

--- index.js
+++ index.js
@@ -28,19 +28,45 @@ var data = [
     Hiringstatus:"completed",
     HRComments:"ok",
     Actions:"done"
+  },
+  {
+    requestId: 3,
+    projectName: "Proj-1",
+    projectManager: "Man-1",
+    resources:4,
+    Designation:"d-1",
+    status:"open",
+    Pointofcontact:"HR-1",
+    Hiringstatus:"completed",
+    HRComments:"ok",
+    Actions:"done"
   }
 ];
+function editDetails(e){
+    var $req = $(e.target);
+    var reqId = $req.attr("id");
+    console.log("request id: " +  reqId);
+
+    //var reqObj = data.filter(o => o.requestId == reqId);
+    //console.log({ details: reqObj });
+
+    // Add a new row (details module) below the selected row in the table.
+
+  };
 function appendrow(){
   for(let i=0;i<data.length;i++)
   {
     var txt="<tr>";
-    for(let j=0;j<Object.keys(data[i]).length;j++)
+    for(let j=0;j<Object.keys(data[i]).length-1;j++)
     {
       txt=txt+"<td>"+String(data[i][Object.keys(data[i])[j]])+"</td>";
     }
+    txt=txt+'<td id="'+String(data[i]["requestId"])+'" class="edit-action"><i class="fa fa-bars" aria-hidden="true"></i></td>'
       txt=txt+"</tr>";
     $(".mytable tbody").append(txt);
+
   }
+  $(".edit-action").click(editDetails);
 
 };
 function showLogin(){
